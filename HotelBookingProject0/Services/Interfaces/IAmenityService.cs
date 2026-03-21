0726f7b8361db5373c5ada3ad60cb5b7eb8d2aea
data <datazazashvili1@gmail.com>
2026-03-22 00:04:30 +0400

    Adding IAmenityService

---
+++ HotelBookingProject0/Services/Interfaces/IAmenityService.cs
@@ -0,0 +1,14 @@
+﻿using HotelBookingProject0.Models.DTO;
+
+namespace HotelBookingProject0.Services.Interfaces
+{
+    public interface IAmenityService
+    {
+        /// <summary>
+        /// this method gets Amenity by Name
+        /// </summary>
+
+
+        Task<AmenityDTO?> GetAmenityByNameAsync(string name);
+    }
+}
